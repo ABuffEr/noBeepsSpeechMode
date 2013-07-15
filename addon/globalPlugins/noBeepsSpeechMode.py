7eed1d1298857855841c34017ad380f80e4c6a84
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-07-15 20:05:44 +0200

    Initial commit.

---
+++ addon/globalPlugins/noBeepsSpeechMode.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+#Plugin to switch only between Talk or Off speech modes
+#Author: Alberto Buffolino
+#Last update: 13.03.2013
+
+import addonHandler
+import globalPluginHandler
+import ui
+import speech
+addonHandler.initTranslation()
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+
+	def script_noBeepsSpeechMode(self, gesture):
+		curMode=speech.speechMode
+		if curMode==speech.speechMode_talk:
+			# change order of two next lines of code to having no speech message about off
+			# (direct off mode, with only Braille message, if active)
+			ui.message(_("speech mode %s")%_("off"))
+			speech.speechMode=speech.speechMode_off
+		elif curMode==speech.speechMode_off:
+			speech.speechMode=speech.speechMode_talk
+			ui.message(_("speech mode %s")%_("talk"))
+
+	script_noBeepsSpeechMode.__doc__=_("Toggles between the speech modes of off and talk. When set to off NVDA will not speak anything. If talk then NVDA wil just speak normally.")
+
+	__gestures = {
+		"kb:NVDA+s": "noBeepsSpeechMode",
+	}
