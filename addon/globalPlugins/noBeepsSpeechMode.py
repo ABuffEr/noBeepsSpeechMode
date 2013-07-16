21b720c4f99164e2e730fa42c6ba16253cc01fbe
Noelia Ruiz Martínez <nrm1977@gmail.com>
2013-07-16 07:02:09 +0200

    Added comments for translators.

--- addon/globalPlugins/noBeepsSpeechMode.py
+++ addon/globalPlugins/noBeepsSpeechMode.py
@@ -16,12 +16,14 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 		if curMode==speech.speechMode_talk:
 			# change order of two next lines of code to having no speech message about off
 			# (direct off mode, with only Braille message, if active)
+			# Translators: A speech mode which disables speech output.
 			ui.message(_("speech mode %s")%_("off"))
 			speech.speechMode=speech.speechMode_off
 		elif curMode==speech.speechMode_off:
 			speech.speechMode=speech.speechMode_talk
+			# Translators: The normal speech mode; i.e. NVDA will talk as normal.
 			ui.message(_("speech mode %s")%_("talk"))
-
+	# Translators: Message presented in input help mode.
 	script_noBeepsSpeechMode.__doc__=_("Toggles between the speech modes of off and talk. When set to off NVDA will not speak anything. If talk then NVDA wil just speak normally.")
 
 	__gestures = {
