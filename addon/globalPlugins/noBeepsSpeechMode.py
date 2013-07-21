009d269cd22a756405d71cb25d077cc994dccd57
Joseph Lee <joseph.lee22590@gmail.com>
2013-07-20 17:53:22 -0700

    code clarification: moved comments and code around speech messages; clarified translator comments.

--- addon/globalPlugins/noBeepsSpeechMode.py
+++ addon/globalPlugins/noBeepsSpeechMode.py
@@ -13,16 +13,16 @@ class GlobalPlugin(globalPluginHandler.GlobalPlugin):
 
 	def script_noBeepsSpeechMode(self, gesture):
 		curMode=speech.speechMode
+		# change order of two next lines of code to having no speech message about off
+		# (direct off mode, with only Braille message, if active)
 		if curMode==speech.speechMode_talk:
-			# change order of two next lines of code to having no speech message about off
-			# (direct off mode, with only Braille message, if active)
-			# Translators: A speech mode which disables speech output.
-			ui.message(_("speech mode %s")%_("off"))
+			# Translators: A speech mode which disables speech output (copy of "speech mode talk" from regular nvda.po message).
+			ui.message(_("speech mode off"))
 			speech.speechMode=speech.speechMode_off
 		elif curMode==speech.speechMode_off:
 			speech.speechMode=speech.speechMode_talk
-			# Translators: The normal speech mode; i.e. NVDA will talk as normal.
-			ui.message(_("speech mode %s")%_("talk"))
+			# Translators: The normal speech mode; i.e. NVDA will talk as normal (copy of "speech mode talk" from regular nvda.po message).
+			ui.message(_("speech mode talk"))
 	# Translators: Message presented in input help mode.
 	script_noBeepsSpeechMode.__doc__=_("Toggles between the speech modes of off and talk. When set to off NVDA will not speak anything. If talk then NVDA wil just speak normally.")
 
