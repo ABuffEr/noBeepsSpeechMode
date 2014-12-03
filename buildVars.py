c76d2c6fd4c2e2996954521e3e676f753235a1be
Noelia Ruiz Martínez <nrm1977@gmail.com>
2014-12-03 19:17:26 +0100

    Bump version to 2.1: translations updates.

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Add-on to exclude Beeps from possible speech modes pressing NVDA+s"""),
 	# version
-	"addon_version" : "2.0",
+	"addon_version" : "2.1",
 	# Author(s)
 	"addon_author" : u"Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
