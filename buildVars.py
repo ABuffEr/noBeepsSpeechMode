156010e4e28ce176a633949cb0e37b337a86e3d9
ABuffEr <a.buffolino@gmail.com>
2023-08-05 21:38:09 +0200

    Bump version, adjusted buildVars, fixed readme links

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Add-on to exclude Beeps from possible speech modes pressing NVDA+s"""),
 	# version
-	"addon_version": "5.3",
+	"addon_version": "5.4",
 	# Author(s)
 	"addon_author": "Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,13 +37,13 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2021.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.1",
+	"addon_lastTestedNVDAVersion": "2023.2",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
 	"addon_updateChannel": None,
 	# Add-on license such as GPL 2
-	"addon_license": None,
+	"addon_license": "GPL v2",
 	# URL for the license document the ad-on is licensed under
 	"addon_licenseURL": None,
 }
--- readme.md
+++ readme.md
@@ -10,5 +10,6 @@ You can only toggle between off or talk.
 Note: current release is for NVDA 2021.1 and beyond; for older versions, download the [4.0 stable][old-stable]
 
 
-[old-stable]: https://github.com/ABuffEr/noBeepsSpeechMode/releases/download/v4.0/noBeepsSpeechMode-4.0.nvda-addon
-[stable]: https://github.com/ABuffEr/noBeepsSpeechMode/releases/download/v5.3/noBeepsSpeechMode-5.3.nvda-addon
+[old-stable]: https://www.nvaccess.org/addonStore/legacy?file=noBeepsSpeechMode-old
+
+[stable]: https://www.nvaccess.org/addonStore/legacy?file=noBeepsSpeechMode
