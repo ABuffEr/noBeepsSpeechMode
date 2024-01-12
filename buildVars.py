c61d022eb68279291718c31b5e1a7be5b1b654f8
ABuffEr <a.buffolino@gmail.com>
2024-01-12 19:23:36 +0100

    Updated version and compatibility flag

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Add-on to exclude Beeps from possible speech modes pressing NVDA+s"""),
 	# version
-	"addon_version": "5.4",
+	"addon_version": "5.5",
 	# Author(s)
 	"addon_author": "Alberto Buffolino <a.buffolino@gmail.com>",
 	# URL for the add-on documentation support
@@ -37,7 +37,7 @@ addon_info = {
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
 	"addon_minimumNVDAVersion": "2021.1",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2023.2",
+	"addon_lastTestedNVDAVersion": "2024.1",
 	# Add-on update channel (default is None, denoting stable releases,
 	# and for development releases, use "dev".)
 	# Do not change unless you know what you are doing!
